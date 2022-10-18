f67ba2911ff91ea8aad39abd616d62eedc2d7208
Maria Cristina Ayala <maria.ayala.mt@usa.edu.co>
2022-10-17 23:30:23 -0500

    first commit

---
+++ src/main/java/com/example/demo/Repositorio/MessageRepository.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.demo.Repositorio;
+
+import com.example.demo.Interface.MessageInterface;
+import org.springframework.stereotype.Repository;
+import com.example.demo.Modelo.Message;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author Usuario
+ */
+@Repository
+public class MessageRepository {
+    @Autowired
+    private MessageInterface extensionesCrud;
+    
+    public List<Message> getAll(){
+        return (List<Message>)extensionesCrud.findAll();
+   }
+   
+    public Optional<Message> getMessage(int id){
+        return extensionesCrud.findById(id);
+    }
+    
+    public Message save(Message message){
+        return extensionesCrud.save(message);
+    }
+    
+     public void delete(Message message){
+        extensionesCrud.delete(message);
+    }
+}
---
+++ src/main/java/com/example/demo/Reto3G36Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto3G36Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto3G36Application.class, args);
+	}
+
+}
